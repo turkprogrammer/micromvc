b8d1cd0f6fdeb8e9af737098f266094753b2e659
Robert <ostbat43@gmail.com>
2019-03-18 14:20:23 +0500

    Router class init

--- application/core/Router.php
+++ application/core/Router.php
@@ -13,24 +13,49 @@ class Router {
     protected $routes = [];
     protected $params = [];
 
-    function __construct() {
-       $arr = require 'application/config/routes.php'; // loading config
-      // debug($arr);
-      foreach ($arr as $key => $value) {
-          $this->add($key, $value);
-      }
+    public function __construct() {
+        $arr = require 'application/config/routes.php'; // loading config
+        // debug($arr);
+        foreach ($arr as $key => $val) {
+            $this->add($key, $val);
+        }
     }
 
     public function add($route, $params) {
-       $route = '';
+        $route = '#^' . $route . '$#';
+        $this->routes[$route] = $params;
     }
 
     public function match() {
-        
+        $url = trim($_SERVER['REQUEST_URI'], '/');
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
+                $this->params = $params;
+                return true;
+            }
+        }
+        return false;
     }
 
     public function run() {
-        echo 'Run';
+        if ($this->match()) {
+            $path = 'application\controllers\\' . ucfirst($this->params['controller']) . 'Controller';
+           
+            if (class_exists($path)) {
+                $action = $this->params['action'] . 'Action';
+                if (method_exists($path, $action)) {
+                   $controller = new $path($this->params);
+                    $controller->$action();
+                } else {
+                    echo 'Метод не найден: ' . $action;
+                }
+            } else {
+                echo 'Не найден контроллер: ' . $path;
+            }
+        } else {
+            echo 'Маршрут не найден'.$_SERVER['REQUEST_URI'];
+           
+        }
     }
 
 }
